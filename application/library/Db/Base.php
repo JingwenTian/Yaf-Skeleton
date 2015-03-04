87099a40c1443f494931d57ae43d1482ba364ae7
JingwenTian <mini.mosquitor@gmail.com>
2015-03-04 17:54:58 +0800

    支持跨库操作

--- application/library/Db/Base.php
+++ application/library/Db/Base.php
@@ -1,7 +1,22 @@
 <?php
 class Db_Base
 {
+	protected $_db;
+	protected $_config = '';
+	
+	//应用于单库操作,在Bootstrap.php初始化DB类
+	//public function __construct() {
+	//	$this->_db = Yaf_Registry::get("Db");
+	//}
+	
+	//支持跨库操作
+	
 	public function __construct() {
-		$this->_db = Yaf_Registry::get("Db");
-	}	
+		
+		$pConfig = $this->_config;
+		$database = Yaf_Registry::get("config")->database->$pConfig->toArray();
+		$this->_db = new Db_Medoo($database);
+		
+	}
+	
 }
